a03226b2e61d5748eece0caf98fcf66fa420ed8e
dean <hanmilo@gmail.com>
2025-01-09 13:06:52 +0800

    Initial Commit

---
+++ pizzanow/pizzanowApp.swift
@@ -0,0 +1,17 @@
+//
+//  pizzanowApp.swift
+//  pizzanow
+//
+//  Created by Dean on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct pizzanowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pizzanowTests/pizzanowTests.swift
@@ -0,0 +1,17 @@
+//
+//  pizzanowTests.swift
+//  pizzanowTests
+//
+//  Created by Dean on 9/1/25.
+//
+
+import Testing
+@testable import pizzanow
+
+struct pizzanowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
