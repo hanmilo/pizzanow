1fe886dd47615b61564e4d536701b95934a1d248
dean <hanmilo@gmail.com>
2025-01-10 18:56:34 +0800

    Added "RatingView" and Updated UI

---
+++ pizzanow/RatingsView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingsView.swift
+//  pizzanow
+//
+//  Created by Dean on 10/1/25.
+//
+
+import SwiftUI
+
+struct RatingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingsView()
+}
